f9acd4e65a841afde8e2111b80207335736534f2
Pyae Sone <myothantktt@gmail.com>
2017-11-03 03:35:17 +0630

    Phone call, Send Email and Camera implicit intent sample

--- app/src/main/java/com/example/pyaesone/padc_implicitintent/MainActivity.java
+++ app/src/main/java/com/example/pyaesone/padc_implicitintent/MainActivity.java
@@ -1,19 +1,33 @@
 package com.example.pyaesone.padc_implicitintent;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button sharingWithShareCompatBtn, navOnMapBtn, phCallBtn, sendEmailBtn, cameraBtn,
             selectImgBtn, eventOnCalendarBtn;
 
+    private Context content;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        content = this;
 
         findViewsByID();
 
@@ -48,21 +62,43 @@ public class MainActivity extends AppCompatActivity {
         phCallBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent phoneCallInt = new Intent(Intent.ACTION_CALL);
+                phoneCallInt.setData(Uri.parse("tel:09794805399"));
+                if (ActivityCompat.checkSelfPermission(content, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
 
+
+
+                    return;
+                }
+                startActivity(phoneCallInt);
             }
         });
 
         sendEmailBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent sendEmailInt = new Intent(Intent.ACTION_SEND);
+                sendEmailInt.setData(Uri.parse("mailto:"));
+                sendEmailInt.setType("text/plain");
+                startActivity(sendEmailInt);
             }
         });
 
         cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent cameraInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (cameraInt.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(cameraInt,REQUEST_IMAGE_CAPTURE);
+                }
+                startActivity(cameraInt);
             }
         });
 
